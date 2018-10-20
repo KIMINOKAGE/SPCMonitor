95303dfa671c0534641aa7aec57a7608c325b9a9
KIMINOKAGE <937802080@qq.com>
2018-10-20 11:44:19 +0800

    1

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SPCMonitor.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginModel)
+        {
+            return View();
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
